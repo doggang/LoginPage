2d4863bbee0fdf80294528532945194171039570
doggang <rlaehgusqp@naver.com>
2023-11-15 22:16:58 +0900

    update

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ window.onload = function(){
   var form = document.getElementById('login');
   var inputId = document.getElementById('inputId');
   var inputPw = document.getElementById('inputPw');
-  
+
   function onLogin(e){
     const userId = inputId.value;
     const userPw = inputPw.value;
     if(userId === "rlaehgusqp" && userPw === "1234"){
-      alert("정답");
+      document.getElementById('again').className+='hidden';
     }else{
       e.preventDefault();
-      alert("떙");
+      document.getElementById('again').classList.remove('hidden');
     }
   }
   
